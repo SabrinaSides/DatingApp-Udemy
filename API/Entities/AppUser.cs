043b425aa63df93ca74d5cdc556183d49081c272
Sabrina Sides <sabrina.sides@hobbylobby.com>
2021-09-08 11:00:48 -0500

    .NET skeleton

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //can be "get" or "set" from other classes in the app
+       public int Id { get; set; } 
+
+       public string UserName { get; set; }
+       
+    }
+}
